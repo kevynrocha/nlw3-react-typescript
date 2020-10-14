d5cc66aa1e6731c284363aae780d52fb454a2c89
Kevyn Oliveira <kevyn_oliver@hotmail.com>
2020-10-13 22:29:56 -0400

    feat: add and config routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Routes from './routes';
+import GlobalStyles from './styles/global';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Certo</h1>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <Routes />
+    <GlobalStyles />
+  </>
+);
 
 export default App;
