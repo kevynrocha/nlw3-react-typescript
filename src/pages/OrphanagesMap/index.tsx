1d0c1683324436154f0d41fffa399508fdf3538b
Kevyn Oliveira <kevyn_oliver@hotmail.com>
2020-10-20 22:45:01 -0400

    feat: add maker and styles on map

--- src/pages/OrphanagesMap/index.tsx
+++ src/pages/OrphanagesMap/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Map, TileLayer } from 'react-leaflet';
+import { Link } from 'react-router-dom';
+import { FiPlus, FiArrowRight } from 'react-icons/fi';
+import { Map, TileLayer, Marker } from 'react-leaflet';
+import Leaflet from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
-import { FiPlus } from 'react-icons/fi';
 import MapMarker from '../../assets/images/MapMarker.svg';
 
 import {
@@ -14,8 +16,15 @@ import {
   Footer,
   City,
   State,
-  Link,
+  Popup,
 } from './styles';
+
+const mapIcon = Leaflet.icon({
+  iconUrl: MapMarker,
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
 
 const mapStyles = {
   width: '100%',
@@ -32,8 +41,8 @@ const OrphanagesMap: React.FC = () => (
         <SubTitle>Muitas crianças estão esperando a sua visita :)</SubTitle>
       </Header>
       <Footer>
-        <City>Rio do Sul</City>
-        <State>Santa Catarina</State>
+        <City>Campo Grande</City>
+        <State>Mato Grosso do Sul</State>
       </Footer>
     </Aside>
 
@@ -41,6 +50,14 @@ const OrphanagesMap: React.FC = () => (
       <TileLayer
         url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
       />
+      <Marker icon={mapIcon} position={[-20.5227256, -54.6424034]}>
+        <Popup closeButton={false} minWidth={240} maxWidth={240}>
+          Lar das meninas
+          <Link to="/">
+            <FiArrowRight size={20} color="#fff" />
+          </Link>
+        </Popup>
+      </Marker>
     </Map>
 
     <Link to="/create">
--- src/pages/OrphanagesMap/styles.ts
+++ src/pages/OrphanagesMap/styles.ts
@@ -1,5 +1,41 @@
 import styled from 'styled-components';
 import { Link as HappyLink } from 'react-router-dom';
+import { Popup as LeafletPopup, PopupProps } from 'react-leaflet';
+
+export const Popup = styled(LeafletPopup)<PopupProps>`
+  .leaflet-popup-content {
+    color: #0089a5;
+    font-size: 20px;
+    font-weight: bold;
+    margin: 8px 12px;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .leaflet-popup-content a {
+    width: 40px;
+    height: 40px;
+    background: #15c3d6;
+    box-shadow: 17.2868px 27.6589px 41.4884px rgba(23, 142, 166, 0.16);
+    border-radius: 12px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .leaflet-popup-content-wrapper {
+    background: rgba(255, 255, 255, 0.8);
+    border-radius: 20px;
+    box-shadow: none;
+  }
+
+  .leaflet-popup-tip-container {
+    display: none;
+  }
+`;
 
 export const Wrapper = styled.div`
   width: 100vw;
