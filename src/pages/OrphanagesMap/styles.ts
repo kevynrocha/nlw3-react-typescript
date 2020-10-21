115e0dc3fd3290aea8fe06818919ea05b394a2e4
Kevyn Oliveira <kevyn_oliver@hotmail.com>
2020-10-20 23:21:30 -0400

    refactor: change css create orphanage button

--- src/pages/OrphanagesMap/styles.ts
+++ src/pages/OrphanagesMap/styles.ts
@@ -1,41 +1,6 @@
 import styled from 'styled-components';
-import { Link as HappyLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Popup as LeafletPopup, PopupProps } from 'react-leaflet';
-
-export const Popup = styled(LeafletPopup)<PopupProps>`
-  .leaflet-popup-content {
-    color: #0089a5;
-    font-size: 20px;
-    font-weight: bold;
-    margin: 8px 12px;
-
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-
-  .leaflet-popup-content a {
-    width: 40px;
-    height: 40px;
-    background: #15c3d6;
-    box-shadow: 17.2868px 27.6589px 41.4884px rgba(23, 142, 166, 0.16);
-    border-radius: 12px;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .leaflet-popup-content-wrapper {
-    background: rgba(255, 255, 255, 0.8);
-    border-radius: 20px;
-    box-shadow: none;
-  }
-
-  .leaflet-popup-tip-container {
-    display: none;
-  }
-`;
 
 export const Wrapper = styled.div`
   width: 100vw;
@@ -82,7 +47,42 @@ export const City = styled.strong`
 
 export const State = styled.span``;
 
-export const Link = styled(HappyLink)`
+export const Popup = styled(LeafletPopup)<PopupProps>`
+  .leaflet-popup-content {
+    color: #0089a5;
+    font-size: 20px;
+    font-weight: bold;
+    margin: 8px 12px;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .leaflet-popup-content a {
+    width: 40px;
+    height: 40px;
+    background: #15c3d6;
+    box-shadow: 17.2868px 27.6589px 41.4884px rgba(23, 142, 166, 0.16);
+    border-radius: 12px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .leaflet-popup-content-wrapper {
+    background: rgba(255, 255, 255, 0.8);
+    border-radius: 20px;
+    box-shadow: none;
+  }
+
+  .leaflet-popup-tip-container {
+    display: none;
+  }
+`;
+
+export const CreateButton = styled(Link)`
   position: absolute;
   right: 40px;
   bottom: 40px;
