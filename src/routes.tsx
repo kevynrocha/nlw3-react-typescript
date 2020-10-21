9e49c516df700e11dbf38335e5eab0decf1fec9d
Kevyn Oliveira <kevyn_oliver@hotmail.com>
2020-10-20 23:00:59 -0400

    refactor: new routes - create and details

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,12 +3,16 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import OrphanagesMap from './pages/OrphanagesMap';
+import Orphanage from './pages/Orphanage';
+import CreateOrphanage from './pages/CreateOrphanage';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" component={Home} exact />
-      <Route path="/orphanages" component={OrphanagesMap} exact />
+      <Route path="/app" component={OrphanagesMap} />
+      <Route path="/orphanages/create" component={CreateOrphanage} />
+      <Route path="/orphanages/:id" component={Orphanage} />
     </Switch>
   </BrowserRouter>
 );
