abf73fbad196a50b3b3b3764261428a7f714a3fb
Kevyn Oliveira <kevyn_oliver@hotmail.com>
2020-10-22 00:06:01 -0400

    refactor: change map

--- src/pages/OrphanagesMap/index.tsx
+++ src/pages/OrphanagesMap/index.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus, FiArrowRight } from 'react-icons/fi';
-import { Map, TileLayer, Marker } from 'react-leaflet';
-import Leaflet from 'leaflet';
-import 'leaflet/dist/leaflet.css';
+import { Marker } from 'react-leaflet';
+
+import api from '../../services/api';
 
 import MapMarker from '../../assets/images/map-marker.svg';
-
+import mapIcon from '../../components/Map/happMapIcon';
+import Map from '../../components/Map';
 import {
   Wrapper,
   Aside,
@@ -20,51 +21,61 @@ import {
   CreateButton,
 } from './styles';
 
-const mapIcon = Leaflet.icon({
-  iconUrl: MapMarker,
-  iconSize: [58, 68],
-  iconAnchor: [29, 68],
-  popupAnchor: [170, 2],
-});
+interface Orphanage {
+  id: number;
+  name: string;
+  latitude: number;
+  longitude: number;
+}
 
-const mapStyles = {
-  width: '100%',
-  height: '100%',
-  zIndex: 0,
+const OrphanagesMap: React.FC = () => {
+  const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
+
+  const fetchOrphanages = useCallback(async () => {
+    const response = await api.get('orphanages');
+    setOrphanages(response.data);
+  }, []);
+
+  useEffect(() => {
+    fetchOrphanages();
+  }, [fetchOrphanages]);
+
+  return (
+    <Wrapper>
+      <Aside>
+        <Header>
+          <img src={MapMarker} alt="Happy" />
+          <Title>Escolha um orfanato no mapa</Title>
+          <SubTitle>Muitas crianças estão esperando a sua visita :)</SubTitle>
+        </Header>
+        <Footer>
+          <City>Campo Grande</City>
+          <State>Mato Grosso do Sul</State>
+        </Footer>
+      </Aside>
+
+      <Map>
+        {orphanages.map((orphanage) => (
+          <Marker
+            key={orphanage.id}
+            icon={mapIcon}
+            position={[orphanage.latitude, orphanage.longitude]}
+          >
+            <Popup closeButton={false} minWidth={240} maxWidth={240}>
+              {orphanage.name}
+              <Link to={`/orphanages/${orphanage.id}`}>
+                <FiArrowRight size={20} color="#fff" />
+              </Link>
+            </Popup>
+          </Marker>
+        ))}
+      </Map>
+
+      <CreateButton to="/orphanages/create">
+        <FiPlus size={32} />
+      </CreateButton>
+    </Wrapper>
+  );
 };
-
-const OrphanagesMap: React.FC = () => (
-  <Wrapper>
-    <Aside>
-      <Header>
-        <img src={MapMarker} alt="Happy" />
-        <Title>Escolha um orfanato no mapa</Title>
-        <SubTitle>Muitas crianças estão esperando a sua visita :)</SubTitle>
-      </Header>
-      <Footer>
-        <City>Campo Grande</City>
-        <State>Mato Grosso do Sul</State>
-      </Footer>
-    </Aside>
-
-    <Map center={[-20.5227256, -54.6424034]} zoom={15} style={mapStyles}>
-      <TileLayer
-        url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
-      />
-      <Marker icon={mapIcon} position={[-20.5227256, -54.6424034]}>
-        <Popup closeButton={false} minWidth={240} maxWidth={240}>
-          Lar das meninas
-          <Link to="/orphanages/1">
-            <FiArrowRight size={20} color="#fff" />
-          </Link>
-        </Popup>
-      </Marker>
-    </Map>
-
-    <CreateButton to="/orphanages/create">
-      <FiPlus size={32} />
-    </CreateButton>
-  </Wrapper>
-);
 
 export default OrphanagesMap;
